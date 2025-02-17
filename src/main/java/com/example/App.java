1b40ec48f5990074f0b432c931b31a59410657f5
Jesper Jensen <jesper@jnsn.dev>
2025-02-17 12:45:40 +0100

    some whatever changes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,17 +1,20 @@
 package com.example;
 
+import java.io.PrintWriter;
 import java.util.Properties;
 
 import org.eclipse.persistence.config.PersistenceUnitProperties;
 
-import com.ibm.db2.jcc.DB2Driver;
+import com.ibm.db2.jcc.DB2BaseDataSource;
+import com.ibm.db2.jcc.DB2TraceManager;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        DB2TraceManager.getTraceManager().setLogWriter(new PrintWriter(System.out), DB2BaseDataSource.TRACE_STATEMENT_CALLS | DB2BaseDataSource.TRACE_CONNECTION_CALLS);
         Properties prop = new Properties();
         prop.setProperty(PersistenceUnitProperties.JDBC_URL, "jdbc:db2://localhost:50000/DB");
         prop.setProperty(PersistenceUnitProperties.JDBC_USER, "db2inst1");
